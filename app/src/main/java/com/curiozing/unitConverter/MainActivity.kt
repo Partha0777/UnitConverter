368bb06d62db11227f0e204bcbe3705b4d857150
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-01-26 23:12:20 +0530

    #Added Unit converter basic UI and need to add dropdown and other functionality.

--- app/src/main/java/com/curiozing/unitConverter/MainActivity.kt
+++ app/src/main/java/com/curiozing/unitConverter/MainActivity.kt
@@ -3,13 +3,35 @@ package com.curiozing.unitConverter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.curiozing.unitConverter.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,9 +39,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    UnitConverterUi()
                 }
             }
         }
@@ -27,17 +51,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun UnitConverterUi() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Unit Converter", fontSize = 20.sp)
+        Spacer(modifier = Modifier.height(20.dp))
+        OutlinedTextField(
+            value = "Select", onValueChange = {}, modifier = Modifier
+                .padding(12.dp)
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()) {
+            TextButton(onClick = { /*TODO*/ }, border = BorderStroke(width = 1.dp, color = Color.Gray)) {
+                Text(text = "Select")
+                Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = null)
+            }
+            Spacer(modifier = Modifier.width(100.dp))
+            TextButton(onClick = { /*TODO*/ }, border = BorderStroke(width =1.dp, color = Color.Gray)) {
+                Text(text = "Select")
+                Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = null)
+
+            }
+        }
+    }
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        UnitConverterUi()
     }
 }
