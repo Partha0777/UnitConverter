97abf09b31b2e3005a4d9dc5146f5dd85709b029
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-01-28 21:28:10 +0530

    #Changed Dropdown border UI in for Unit convertor.

--- app/src/main/java/com/curiozing/unitConverter/MainActivity.kt
+++ app/src/main/java/com/curiozing/unitConverter/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,6 +16,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.MailOutline
@@ -35,6 +37,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -89,7 +92,7 @@ fun UnitConverterUi() {
             Box {
                 TextButton(onClick = {
                     inputExpand = true
-                }, border = BorderStroke(width = 1.dp, color = Color.Gray)) {
+                }, modifier = Modifier.border(BorderStroke(width = 1.dp, color = Color.Gray), shape = RoundedCornerShape(8.dp))) {
                     Text(text = selectedInputConverter)
                     Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = null)
                 }
@@ -113,15 +116,12 @@ fun UnitConverterUi() {
                 }
             }
             Spacer(modifier = Modifier.width(100.dp))
-            TextButton(
-                onClick = { /*TODO*/ },
-                border = BorderStroke(width = 1.dp, color = Color.Gray)
-            ) {
-                Text(text = "Select")
+            TextButton(onClick = {
+                inputExpand = true
+            }, modifier = Modifier.border(BorderStroke(width = 1.dp, color = Color.Gray), shape = RoundedCornerShape(8.dp))) {
+                Text(text = selectedInputConverter)
                 Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = null)
-
-            }
-        }
+            }        }
 
 
     }
